bac1efdf4f45573ce7111e80baea7aabcdc37e60
Ruth School <ruthschool@Ruths-MacBook-Air.local>
2023-01-11 12:43:25 +0000

    Calculations complete and working

--- SetUp/PackageInstall.py
+++ SetUp/PackageInstall.py
@@ -1,12 +1,11 @@
 #TRENDING PYTHON PACKAGES
 import pandas as pd 
 import numpy as np 
-import sklearn as sk
+import sklearn 
 import matplotlib.pyplot as plt
 import seaborn as sns
 import plotly.express as px
 import plotly.graph_objects as go
-import spicy
 
 import random
 from plotly.subplots import make_subplots
@@ -16,3 +15,14 @@ import warnings
 from warnings import filterwarnings
 warnings.filterwarnings('ignore')
 
+
+#SK Learn 
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
+from sklearn.metrics import accuracy_score
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import train_test_split
